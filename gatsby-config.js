3c0de7abebf6718136268cc60bd6c8031ccbba0f
Suejin Kim <kim.suejin.20@gmail.com>
2023-01-25 00:04:00 -0500

    clean up plugins text

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,24 @@ module.exports = {
     title: `helx-marketing-site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-PEWT74G972"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        "trackingId": "G-PEWT74G972"
+      }
+    }, 
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }, 
+    "gatsby-plugin-material-ui"
+  ]
 };

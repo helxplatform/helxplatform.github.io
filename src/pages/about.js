85bd1eba3fa714a6fb55789e86c90bf2db380180
suejinkim20 <kim.suejin.20@gmail.com>
2023-02-15 14:22:46 -0500

    add hero component to about and features pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import { Typography } from '@mui/material'
-import { history, institutions } from '../content'
+import { aboutHero, history, institutions } from '../content'
+import { Hero } from '../components/hero/hero'
 import { IllustrationTextSection, InstitutionList } from '../components/sections'
 
 const AboutPage = () => {
   return (
     <Fragment>
       <Typography variant='h1'>About Page</Typography>
+      <Hero content ={aboutHero}/>
       <IllustrationTextSection content={history}/>
       <InstitutionList content={institutions}/>
 
--- src/pages/features.js
+++ src/pages/features.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import { Typography } from '@mui/material'
-import { helxFunctions, useCases } from '../content'
+import { featuresHero, helxFunctions, useCases } from '../content'
+import { Hero } from '../components/hero/hero'
 import { FunctionsSection, UseCasesSection } from '../components/sections/'
 
 const FeaturesPage = () => {
   return (
     <Fragment>
       <Typography variant='h1'>Features Page</Typography>
+      <Hero content={featuresHero}/>
       <FunctionsSection content={helxFunctions}/>
       <UseCasesSection content={useCases}/>
 

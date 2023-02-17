c9db1200bfcc0af3162e64118509c3b1001fa7ab
David Glymph <Woozl@users.noreply.github.com>
2023-02-17 16:47:21 -0500

    initial rotating verb code

--- src/components/sections/rotatingVerbs.js
+++ src/components/sections/rotatingVerbs.js
@@ -1,15 +1,23 @@
-import React, { Fragment } from 'react'
-import { Typography, Box } from '@mui/material'
-import { Pre } from '../pre'
+import React, { Fragment, useEffect } from "react";
+import { Typography, Box } from "@mui/material";
+import { Pre } from "../pre";
+import { useState } from "react";
 
-export const RotatingVerbs = (content) => { 
+export const RotatingVerbs = ({ content: { heading, verbs, subheading } }) => {
+  const [delay, setDelay] = useState(2000);
+  const [currentVerbIndex, setCurrentVerbIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentVerbIndex(prev => (prev + 1) % verbs.length);
+    }, delay)
+    return () => clearInterval(intervalId);
+  }, [delay, verbs.length])
 
   return (
     <Fragment>
-      <Typography>Rotating Verb Section</Typography>
-      <Pre>
-          { JSON.stringify(content, null, 2) }
-      </Pre>
+      <Typography variant="h1">{heading.replace(/do/g, verbs[currentVerbIndex])}</Typography>
+      <Typography variant="subtitle1">{subheading}</Typography>
     </Fragment>
-  )
-}
+  );
+};

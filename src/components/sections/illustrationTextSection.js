7901fe9c792957a46f65d79010cc5c5a6c5b3c8a
David Glymph <Woozl@users.noreply.github.com>
2023-02-20 10:51:07 -0500

    illustration text section component (home & about pages)

--- src/components/sections/illustrationTextSection.js
+++ src/components/sections/illustrationTextSection.js
@@ -1,15 +1,66 @@
-import React, { Fragment } from 'react'
-import { Typography, Box } from '@mui/material'
-import { Pre } from '../pre'
+import React from "react";
+import { Typography, Box, Grid } from "@mui/material";
+import { styled } from "@mui/system";
 
-export const IllustrationTextSection = (content) => { 
+const AboutCard = styled(Box)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(4),
+  color: theme.palette.text.secondary,
+  height: "100%",
 
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  gap: theme.spacing(2),
+}));
+
+export const IllustrationTextSection = ({ content }) => {
   return (
-    <Fragment>
-      <Typography>Home Illustration-Text Section</Typography>
-      <Pre>
-          { JSON.stringify(content, null, 2) }
-      </Pre>
-    </Fragment>
-  )
-}
+    <Box my="4rem">
+      <Grid container spacing={{ md: 16, sm: 0 }}>
+        {content.reduce((accum, section, index) => {
+          const thisSection = [];
+
+          const IllustrationItem = (
+            <Grid
+              item
+              md={6}
+              sm={12}
+              display={{ md: "block", xs: "none" }}
+              key={`${index}-illustration`}
+            >
+              <img src={section.illustration} style={{ width: "100%" }} />
+            </Grid>
+          );
+
+          const TextItem = (
+            <Grid item md={6} sm={12} key={`${index}-text`}>
+              <AboutCard>
+                <Typography
+                  sx={{
+                    fontSize: "1.5rem",
+                    fontWeight: "bold",
+                    color: "#555555",
+                  }}
+                >
+                  {section.title}
+                </Typography>
+                <Typography>{section.subtitle}</Typography>
+              </AboutCard>
+            </Grid>
+          );
+
+          if (index % 2 === 0) {
+            thisSection.push(IllustrationItem);
+            thisSection.push(TextItem);
+          } else {
+            thisSection.push(TextItem);
+            thisSection.push(IllustrationItem);
+          }
+
+          return [...accum, ...thisSection];
+        }, [])}
+      </Grid>
+    </Box>
+  );
+};
--- src/content/home/homeIllustrations.js
+++ src/content/home/homeIllustrations.js
@@ -6,16 +6,16 @@ export const homeSection = [
   {
     "illustration": home1,
     "title": "HeLx solves technical infrastructure challenges to help researchers focus on their science.",
-    "subheading": "Leave the design and implementation of scalability, cloud resources, and account authentication to HeLx so you and your team can focus on what matters most to you."
+    "subtitle": "Leave the design and implementation of scalability, cloud resources, and account authentication to HeLx so you and your team can focus on what matters most to you."
   },
   {
     "illustration": home2,
     "title": "We bring the right tools for each team in a secure, scalable portal.",
-    "subheading": "Each deployment of HeLx is fully customizable for your domain-specific data-driven needs. No mandatory extras, no surprise addons--just your toolsets and your data."
+    "subtitle": "Each deployment of HeLx is fully customizable for your domain-specific data-driven needs. No mandatory extras, no surprise addons--just your toolsets and your data."
   },
   {
     "illustration": home3,
     "title": "Bring your own research data.",
-    "subheading": "Diving into the new, novel, and unknown? HeLx has you covered. Researchers can implement their own datasets for analyses without concern for data use agreements or access requests."
+    "subtitle": "Diving into the new, novel, and unknown? HeLx has you covered. Researchers can implement their own datasets for analyses without concern for data use agreements or access requests."
   }
 ]

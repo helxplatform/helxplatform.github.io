2f6735cbf75eb4aa5543beedce0eac863a56b3a2
David Glymph <Woozl@users.noreply.github.com>
2023-02-20 15:35:36 -0500

    resource cards component

--- src/components/sections/resourceCards.js
+++ src/components/sections/resourceCards.js
@@ -1,15 +1,62 @@
-import React, { Fragment } from 'react'
-import { Typography, Box } from '@mui/material'
-import { Pre } from '../pre'
+import React from "react";
+import { Typography, Box, Grid, Button, Link } from "@mui/material";
 
-export const ResourceCardSection = (content) => { 
-
+export const ResourceCardSection = ({ content }) => {
   return (
-    <Fragment>
-      <Typography>Resource Card Section</Typography>
-      <Pre>
-          { JSON.stringify(content, null, 2) }
-      </Pre>
-    </Fragment>
-  )
-}
+    <Box my="4rem">
+      <Typography
+        variant="h1"
+        sx={{
+          fontSize: "clamp(2rem, 2rem + 1.6667vw, 3rem)",
+          fontFamily: "Open Sans, sans-serif",
+          color: "#382C56",
+          lineHeight: "1.3",
+          textAlign: "center",
+        }}
+      >
+        Resources
+      </Typography>
+      <Grid container spacing={4} my="2rem">
+        {content.map((resource, index) => (
+          <Grid item md={4} sm={12} xs={12} key={index}>
+            <Link href={resource.link} target="_blank">
+              <Box
+                display="flex"
+                flexDirection={{ md: "column", sm: "row" }}
+                justifyContent={{ sm: "center", xs: "center" }}
+                gap={{ md: "0.5rem", sm: "1rem", xs: "1rem" }}
+                alignItems="center"
+              >
+                <Button
+                  variant="outlined"
+                  sx={{
+                    flex: { md: "revert", sm: "0 1 150px", xs: "0 1 150px" },
+                    width: "100%",
+                    padding: "0",
+                    border: "none",
+                    borderRadius: 0,
+                    "&:hover": { border: "none" },
+                  }}
+                >
+                  <img
+                    src={resource.image}
+                    draggable={false}
+                    style={{ width: "100%" }}
+                  />
+                </Button>
+                <Typography
+                  variant="subtitle1"
+                  sx={{
+                    flex: { md: "revert", sm: "0 0 220px", xs: "0 0 220px" },
+                  }}
+                >
+                  {resource.title}
+                </Typography>
+              </Box>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from 'react'
-import { Typography } from '@mui/material'
 import { resourceCards, questions} from '../content'
 import { ResourceCardSection, FaqsSection } from '../components/sections'
 
 const ResourcesPage = () => {
   return (
     <Fragment>
-      <Typography variant='h1'>Resources Page</Typography>
       <ResourceCardSection content={resourceCards}/>
       <FaqsSection content={questions}/>
     </Fragment>

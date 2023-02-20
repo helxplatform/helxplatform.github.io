2c95e97d9cec8fae8746753bfc177b5aac9dc488
David Glymph <Woozl@users.noreply.github.com>
2023-02-20 10:52:18 -0500

    institution list component (about page)
    using <img /> here again. I also swapped some of the images that were grayscale by default

--- src/components/sections/institutionList.js
+++ src/components/sections/institutionList.js
@@ -1,15 +1,50 @@
-import React, { Fragment } from 'react'
-import { Typography, Box } from '@mui/material'
-import { Pre } from '../pre'
+import React from "react";
+import { Typography, Stack, Box } from "@mui/material";
 
-export const InstitutionList = (content) => { 
-
+export const InstitutionList = ({ content }) => {
   return (
-    <Fragment>
-      <Typography>Institution List Section</Typography>
-      <Pre>
-          { JSON.stringify(content, null, 2) }
-      </Pre>
-    </Fragment>
-  )
-}
+    <Stack gap="2rem" my="4rem">
+      <Typography
+        sx={{
+          fontSize: "clamp(1.5rem, 1.5rem + 1.6667vw, 2.2rem)",
+          fontFamily: "Open Sans, sans-serif",
+          fontWeight: 400,
+          color: "#382C56",
+          lineHeight: "1.3",
+          textAlign: "center",
+        }}
+      >
+        HeLx is made possible through the following funders
+      </Typography>
+      <Stack
+        direction={"row"}
+        gap="2rem"
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {content.map((institution, index) => (
+          <Box
+            key={index}
+            sx={{
+              filter: "grayscale(1)",
+              transition: "filter 250ms",
+              margin: "auto 0",
+              "&:hover": {
+                filter: "grayscale(0)",
+                transition: "filter 250ms",
+              },
+            }}
+          >
+            <a href={institution.link} target="_blank">
+              <img
+                src={institution.image}
+                alt={institution.institutionName}
+                style={{ display: "block", maxHeight: 100 }}
+              />
+            </a>
+          </Box>
+        ))}
+      </Stack>
+    </Stack>
+  );
+};

0e2f68c4c02ea8277f0653e51c2981c224a1b0d0
suejinkim20 <kim.suejin.20@gmail.com>
2023-02-22 22:38:00 -0500

    pull out heading content from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import { HomeHero } from '../components/hero/homeHero'
 import { homeSection, rotatingVerbs, helxFeatures }  from '../content'
 import { IllustrationTextSection, RotatingVerbs, FeaturesSection } from '../components/sections'
 
+const headingContent1 = {
+  title: 'Launch, Analyze, and Discover Your Data With HeLx',
+  subtitle: 'HeLx empowers researchers in domains from plant genomics to neuroscience to work with their preferred tools and apps in the cloud at scale.'
+}
+
 const IndexPage = () => {
   return (
     <Fragment>
       <HomeHero/>
-      <Box sx={{ margin: '3rem 18rem 1rem', textAlign: 'center'}}>
-        <Typography variant='h1'>Launch, Analyze, and Discover Your Data With HeLx</Typography>
-        <Typography body1>HeLx empowers researchers in domains from plant genomics to 
-        neuroscience to work with their preferred tools and apps in the cloud at scale.</Typography>
-      </Box>
 
     <IllustrationTextSection content={homeSection}/>
 

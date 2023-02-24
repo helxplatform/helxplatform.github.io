703c0bb887ce8de63ad6224a5e18b27b2ea593cf
David Glymph <Woozl@users.noreply.github.com>
2023-02-24 11:42:46 -0500

    add maxWidthWrapper component, and remove top-level wrapper
    Co-authored-by: Suejin Kim <kim.suejin.20@gmail.com>

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,6 +1,4 @@
 import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
-import { Container } from '@mui/material'
 import { Header } from './header'
 import { Footer } from './footer'
 import { Subfooter } from './subfooter'
@@ -10,9 +8,7 @@ export const Layout = ({ children }) => {
     <Fragment>
         <Header />
       <main>
-        <Container maxWidth="lg">
-          { children }
-        </Container>
+        { children }
       </main>
       <Subfooter />
       <Footer />

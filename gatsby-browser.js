2b61d349a2a1acc8e8925398b375907b31a52af2
David Glymph <Woozl@users.noreply.github.com>
2023-02-27 15:42:21 -0500

    add CssBaseline component back to fix grid spacing on FeaturesSection component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { ThemeProvider } from '@mui/material/styles'
 import { Layout } from './src/components/layout'
 import { theme } from './src/theme'
+import { CssBaseline } from '@mui/material'
 
 export const wrapRootElement = ({ element, props }) => {
   return (
     <ThemeProvider theme={ theme }>
+      <CssBaseline />
       { element }
     </ThemeProvider>
   )

56c2b3173cfbb3379bb05fc75220e88dda305e2c
suejinkim20 <kim.suejin.20@gmail.com>
2023-03-03 14:36:03 -0500

    WIP refactor illustration text section

--- src/components/sections/illustrationTextSection.js
+++ src/components/sections/illustrationTextSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Box, Grid } from "@mui/material";
+import { Typography, Box, Stack } from "@mui/material";
 import { styled } from "@mui/system";
 import { MaxWidthWrapper } from "../layout/maxWidthWrapper";
 
@@ -8,12 +8,60 @@ const AboutCard = styled(Box)(({ theme }) => ({
   padding: theme.spacing(4),
   color: theme.palette.text.secondary,
   height: "100%",
-
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
-  gap: theme.spacing(2),
+  gap: theme.spacing(1),
+  textAlign: 'center',
+  flex: 1,
 }));
+
+const IllustrationItem = ({section}) => (
+  <Box sx={{
+    flex: {
+      sm: '0 0',
+      md: 1
+    },
+    justifyContent: 'center',
+    alignItems: 'center',
+  }}>
+      <img
+      src={section.illustration} 
+      alt={section.illustrationName}
+      style={{ maxWidth: "300px" }} 
+      loading="lazy" />
+  </Box>
+)
+
+const TextItem = ({section}) => (
+  <AboutCard>
+    <Typography variant='h3'>{section.title}</Typography>
+    <Typography variant='body1'>{section.subtitle}</Typography>
+  </AboutCard>
+)
+
+
+ const IllustrationTextSection2 = ({ content }) => {
+  return (
+    <MaxWidthWrapper maxWidth='md'>
+      {
+        content.map((section, index) => {
+          const contentOrder = index % 2 === 0 ? 'row' : 'row-reverse'
+          return (
+            <Stack
+              direction={contentOrder}
+              spacing={6}
+              key={`illustrationSection-${index}`}
+            >
+              <IllustrationItem section={section}/>
+              <TextItem section={section}/>
+            </Stack>
+          )
+        })
+      }
+    </MaxWidthWrapper>
+  )
+}
 
 export const IllustrationTextSection = ({ content }) => {
   return (
@@ -31,11 +79,7 @@ export const IllustrationTextSection = ({ content }) => {
                 display={{ md: "block", xs: "none" }}
                 key={`${index}-illustration`}
               >
-                <img
-                  src={section.illustration} 
-                  alt={section.illustrationName}
-                  style={{ maxWidth: "300px" }} 
-                  loading="lazy" />
+                <img src={section.illustration} style={{ width: "100%" }} loading="lazy" />
               </Grid>
             );
 

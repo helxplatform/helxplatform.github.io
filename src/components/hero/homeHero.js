1d8f1acd8d7835d49c51cc2b669e042cd4500025
suejinkim20 <kim.suejin.20@gmail.com>
2023-03-06 11:20:58 -0500

    WIP home hero

--- src/components/hero/homeHero.js
+++ src/components/hero/homeHero.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Box, Typography, Button } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { MaxWidthWrapper } from "../layout/maxWidthWrapper"
+import { Link } from '../link'
 
 export const HomeHero = ({ content }) => {
   const theme = useTheme()
@@ -11,34 +12,63 @@ export const HomeHero = ({ content }) => {
       <Box
         sx={{
           height: '400px',
-          display: 'flex'
+          display: 'flex',
+          overflow: 'hidden',
         }}
       >
-        <Box sx={{width: '400px', padding: '4rem 3rem 1rem'}}>
-          <Typography variant="h1" sx={{color: theme.palette.branding.offWhite}}>{content.title}</Typography>
-          <Typography variant="body2">
-            {content.subheading}
-          </Typography>
-          <Button variant='filled' size='medium' sx={{
-            color: theme.palette.branding.deepPurple, 
-            textTransform: 'none',
-            backgroundColor: theme.palette.branding.yellow,
-          }}>
-            Why use Helx?
-          </Button>
+        <Box sx={{ 
+          flex: '1 1 200px', 
+          padding: '2rem 0', 
+          display: 'flex', 
+          flexDirection: 'column', 
+          justifyContent: 'space-between',
+        }}>
+          <Box>
+            <Typography variant="h1" sx={{
+              color: theme.palette.branding.offWhite,
+              mb: '1rem',
+            }}>
+              {content.title}
+            </Typography>
+            <Typography variant="body2">
+              {content.subheading}
+            </Typography>
+          </Box>
+          <Box>
+            <Button variant='filled' size='medium' component={ Link } to="/features" sx={{
+              color: theme.palette.branding.deepPurple, 
+              textTransform: 'none',
+              backgroundColor: theme.palette.branding.yellow,
+              '&:hover': {
+                backgroundColor: `${ theme.palette.branding.yellow }70`,
+              }
+            }}>
+              <Typography variant='subtitle2'>
+                Why use Helx?
+              </Typography>
+            </Button>
+
+          </Box>
         </Box>
         <Box sx={{
-          alignItems: 'center',
-          display: 'inline-flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          margin: '0 auto',
-          paddingTop: '7rem',
+          flex: '2 2',
           overflow: 'hidden',
+          position: 'relative',
         }}>
-          <img src={content.image} style={{ margin: '0 auto', width: 400 }} loading="lazy" />
+          <img 
+            src={content.image} 
+            loading="lazy"
+            style={{ 
+              margin: '0 auto', 
+              width: '400px',
+              position: 'absolute',
+              bottom: '-7rem',
+              right: '0'
+            }} 
+          />
         </Box>
       </Box>
+
     </MaxWidthWrapper>    
   )
 }

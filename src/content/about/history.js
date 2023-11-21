ea7d11214740cdec66fc34c82da3574d4be38d15
suejinkim20 <kim.suejin.20@gmail.com>
2023-11-21 12:41:31 -0500

    update history text content

--- src/content/about/history.js
+++ src/content/about/history.js
@@ -6,12 +6,12 @@ export const history = [
     "illustration": about1,
     "illustrationName": "Technical Infrastructure Illustration",
     "title": "HeLx is operated out of the Renaissance Computing Institute (RENCI)",
-    "subtitle": "HeLx was developed across the timeline of Renaissance Computing Institute (RENCI) at the University of North Carolina at Chapel Hill empowering diverse science communities to use advanced computing infrastructures."
+    "subtitle": "HeLx was developed over time as RENCI worked with diverse research  communities to create and implement advanced computing infrastructures."
   },
   {
     "illustration": about2,
     "illustrationName": "Data Science Tools Illustration",
     "title": "Streamlined Data + Research",
-    "subtitle": "We learned that research communities have a wide array of data science tools in their daily toolbox, but provisioning these tools in modern cloud native environments with appropriate security, networking, and persistence support is daunting. HeLx serves as an on-ramp for these communities to assemble the right tools and to help them scale from on-premise to cloud infrastructures."
+    "subtitle": "We learned that research communities have a wide array of data science tools in their daily toolbox, but provisioning these tools in modern cloud-native environments with appropriate security, networking, and persistence support is daunting. HeLx serves as an on-ramp for research communities to assemble the right tools and to help them scale from on-premise to cloud infrastructures."
   }
 ]

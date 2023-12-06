f8b2b167fd307c168d3d358d24cad0f57dbe7472
suejinkim20 <kim.suejin.20@gmail.com>
2023-12-06 14:57:57 -0500

    add links to renci website and helx platform github into footer

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,29 +1,45 @@
-import React, { Fragment } from 'react'
-import { Box, Typography } from '@mui/material'
+import React from 'react'
+import { Box, Typography, Link, Stack } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
+import GitHubIcon from '@mui/icons-material/GitHub';
 import './layout.css'
 
 export const Footer = () => {
   const theme = useTheme()
 
   return (
-    <Fragment>
-      <Box
-        component="footer"
-        sx={{
-          background: theme.palette.branding.offWhite,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          letterSpacing: '0.5px',
-          padding: theme.spacing(2),
-        }}
+    <Box
+      component="footer"
+      sx={{
+        background: theme.palette.branding.offWhite,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        letterSpacing: '0.5px',
+        padding: theme.spacing(2),
+      }}
+    >
+      <Stack
+        direction={{ xs: 'column', md: 'row' }}
+        spacing={{ xs: 1, md: 4 }}
+        sx={{padding: '2rem 0 1.5rem'}}
       >
-        <Typography paragraph align="center">
-          &copy; { new Date().getFullYear() }  Renaissance Computing Institute
-        </Typography>
-      </Box>
-    </Fragment>
+        <Link href="https://renci.org" target="_blank" underline="hover">
+          <Typography paragraph>
+            &copy; { new Date().getFullYear() }  Renaissance Computing Institute
+          </Typography>
+        </Link>      
+        <Link href="https://github.com/helxplatform" target="_blank" underline="hover" >
+          <Stack
+            direction="row"
+            spacing={1}
+          >
+            <GitHubIcon size={12} />
+            <Typography paragraph>HeLx Platform GitHub</Typography>
+          </Stack>
+        </Link>
+      </Stack>
+    </Box>
   )
 }

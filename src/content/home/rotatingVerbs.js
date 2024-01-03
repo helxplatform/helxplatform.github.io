c4ca08b67b804522dfc40883e3aeeb88733964cf
suejinkim20 <kim.suejin.20@gmail.com>
2024-01-03 16:38:19 -0500

    Updates

--- src/content/home/rotatingVerbs.js
+++
@@ -1,5 +0,0 @@
-export const rotatingVerbs = {
-  "heading": "What can you do with HeLx today?",
-  "verbs": [ "build", "analyze", "investigate", "share", "learn"],
-  "subheading": "Reach new data frontiers in your research by using HeLx."
-}

d8d8969304278d43e23dcf3344b79549dc0616a4
suejinkim20 <kim.suejin.20@gmail.com>
2024-11-07 13:10:56 -0500

    add eduhelx content files

---
+++ src/content/eduhelx/aboutEduhelx.js
@@ -0,0 +1,12 @@
+import eduLex from '../../images/eduhelx/eduLex.png'
+
+export const aboutEduhelx = [
+  {
+    "illustration": eduLex,
+    "illustrationName": "Technical Infrastructure Illustration",
+    "title": "Customized for your needs.",
+    "subtitle": `Students learn to use tools that are widely used in Data Science in industry and academic research including Jupyter notebooks and RStudio. Instructors are able to concentrate on pedagogical material without worrying about the computing infrastructure. 
+
+    EduHeLx is similar to Google Colab and Berkeley D8 infrastructure but customized for the unique needs of your classroom, your materials, and your students.`
+  }
+]
---
+++ src/content/eduhelx/eduhelxTestimonials.js
@@ -0,0 +1,15 @@
+import emilyPfaff from '../../images/eduhelx/emily-pfaff.png'
+import stanAhalt from '../../images/eduhelx/stan-ahalt.png'
+
+export const eduHelxTestimonials = [
+  {
+    quote: `EduHeLx was a game changer. It took a lot of the friction out of getting in the database, because you literally just launched it and there you are. You're ready to go.`,
+    name: 'Emily Pfaff, PhD',
+    image: emilyPfaff
+  },
+  {
+    quote: 'Rock solid teaching experience. I enjoyed using the platform!',
+    name: 'Stan Ahalt, PhD',
+    image: stanAhalt,
+  }
+];

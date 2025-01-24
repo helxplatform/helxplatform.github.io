ea9942f05f062284e91e238714efa476753a5964
nathanlf <nathanlf@renci.org>
2025-01-24 14:37:18 -0500

    put features card back into features.js, refactor content to mdx

--- src/components/sections/features.js
+++ src/components/sections/features.js
@@ -4,7 +4,7 @@ import { styled } from "@mui/system";
 import { MaxWidthWrapper } from "../layout/maxWidthWrapper";
 import { useTheme } from "@mui/material";
 
-const FeatureCard = styled(Box)(({ theme }) => ({
+export const FeatureCard = styled(Box)(({ theme }) => ({
   backgroundColor: "#fff",
   padding: theme.spacing(4),
   textAlign: "center",
@@ -17,38 +17,60 @@ const FeatureCard = styled(Box)(({ theme }) => ({
   alignItems: "center",
 }));
 
-
 export const FeaturesSection = ({ content, columns }) => {
   const theme = useTheme();
-  const columnWidth = columns === 3 ? 4 : columns === 2 ? 6 : 12
-  
+  const columnWidth = columns === 3 ? 4 : columns === 2 ? 6 : 12;
+
   return (
-    <MaxWidthWrapper backgroundColor={theme.palette.branding.offWhite} maxWidth='xl' gutterBottom>   
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
+    <MaxWidthWrapper
+      backgroundColor={theme.palette.branding.offWhite}
+      maxWidth="xl"
+      gutterBottom
     >
-      <Grid container spacing={4} pb="4rem" sx={{ maxWidth: "1300px", padding: "32px" }}>
-        {content.map((feature, index) => (
-          <Grid item md={columnWidth} sm={6} xs={12} key={index} alignItems="stretch">
-            <FeatureCard>
-              <Box sx={{ minHeight: 100 }}>
-                <img src={feature.image} style={{ display: "block", height: 75 }} loading="lazy" alt={feature.title} />
-              </Box>
-              <Box>
-                <Typography mb="1rem" sx={{ fontWeight: "bold" }}>
-                  {feature.title}
-                </Typography>
-                <Typography variant='subtitle2'>{feature.subheading}</Typography>
-              </Box>
-            </FeatureCard>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Grid
+          container
+          spacing={4}
+          pb="4rem"
+          sx={{ maxWidth: "1300px", padding: "32px" }}
+        >
+          {content.map((feature, index) => (
+            <Grid
+              item
+              md={columnWidth}
+              sm={6}
+              xs={12}
+              key={index}
+              alignItems="stretch"
+            >
+              <FeatureCard>
+                <Box sx={{ minHeight: 100 }}>
+                  <img
+                    src={feature.image}
+                    style={{ display: "block", height: 75 }}
+                    loading="lazy"
+                    alt={feature.title}
+                  />
+                </Box>
+                <Box>
+                  <Typography mb="1rem" sx={{ fontWeight: "bold" }}>
+                    {feature.title}
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    {feature.subheading}
+                  </Typography>
+                </Box>
+              </FeatureCard>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </MaxWidthWrapper>
   );
 };

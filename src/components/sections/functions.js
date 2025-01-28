aaf3ab377f3d8dfc0605edc410cc779498eb25a9
nathanlf <nathanlf@renci.org>
2025-01-28 14:30:21 -0500

    simplify helxFunctions mdx file & component

--- src/components/sections/functions.js
+++ src/components/sections/functions.js
@@ -2,22 +2,19 @@ import React from "react";
 import { Typography, Grid } from "@mui/material";
 import { MaxWidthWrapper } from "../layout/maxWidthWrapper";
 
-export const FunctionsSection = ({ content }) => {
+export const SectionGrid = ({ children }) => {
   return (
     <MaxWidthWrapper gutterBottom>
-      <Typography variant="h2">
-        What can HeLx do for you?
-      </Typography>
       <Grid container spacing={{ md: 8, sm: 6, xs: 4 }}>
-        {content.map((feature, index) => (
-          <Grid item md={6} xs={12} key={index}>
-            <Typography variant="h3">
-              {feature.heading}
-            </Typography>
-            <Typography variant="body1">{feature.paragraph}</Typography>
-          </Grid>
-        ))}
+        {children}
       </Grid>
     </MaxWidthWrapper>
   );
 };
+
+export const GridItem = ({ heading, children }) => (
+  <Grid item md={6} xs={12}>
+    <Typography variant="h3">{heading}</Typography>
+    <Typography variant="body1">{children}</Typography>
+  </Grid>
+);
--- src/pages/features.js
+++ src/pages/features.js
@@ -1,20 +1,20 @@
-import React, { Fragment } from 'react'
-import { featuresHero, helxFunctions, useCases } from '../content'
-import { Hero } from '../components/hero/hero'
-import { FunctionsSection, UseCasesSection } from '../components/sections/'
-import { Subfooter } from '../components/layout/subfooter'
+import React, { Fragment } from "react";
+import { featuresHero, helxFunctions, useCases } from "../content";
+import { Hero } from "../components/hero/hero";
+import { SectionGrid, UseCasesSection } from "../components/sections/";
+import { Subfooter } from "../components/layout/subfooter";
 
 const FeaturesPage = () => {
   return (
     <Fragment>
-      <Hero content={featuresHero}/>
-      <FunctionsSection content={helxFunctions}/>
-      <UseCasesSection content={useCases}/>
-      <Subfooter/>
+      <Hero content={featuresHero} />
+      <SectionGrid content={helxFunctions} />
+      <UseCasesSection content={useCases} />
+      <Subfooter />
     </Fragment>
-  )
-}
+  );
+};
 
-export default FeaturesPage
+export default FeaturesPage;
 
-export const Head = () => <title>Features</title>
+export const Head = () => <title>Features</title>;

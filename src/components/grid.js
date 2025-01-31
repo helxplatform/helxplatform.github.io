5a77b0d678cb163d8e039729c02d94be542afd34
nathanlf <nathanlf@renci.org>
2025-01-31 15:27:32 -0500

    refactor features, useCases, and functions section components into GridContainer

---
+++ src/components/grid.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+export const GridContainer = ({
+  children,
+  spacing = { md: 8, sm: 6, xs: 4 },
+  padding,
+}) => {
+  return (
+    <Grid container spacing={spacing} padding={padding}>
+      {children}
+    </Grid>
+  );
+};
